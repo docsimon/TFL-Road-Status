1f6890117ae402d6b8347330cba98774907b3c2e
doc <doc@docs-MBP.lan>
2018-05-12 16:10:20 +0100

    Added unit tests for Data Model

--- TFLRoadStatusTests/TFLRoadStatusTests.swift
+++ TFLRoadStatusTests/TFLRoadStatusTests.swift
@@ -11,26 +11,57 @@ import XCTest
 
 class TFLRoadStatusTests: XCTestCase {
     
+    var data: Data?
+    var url: URL?
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let bundle = Bundle(for: type(of: self))
+        guard let murl = bundle.url(forResource: "DataTest", withExtension: "json") else {
+            XCTFail("No file found")
+            return
+        }
+        
+        url = murl
+        data = try? Data(contentsOf: url!)
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testDataParser() {
+        if let data = data {
+            let parsingResult: ([Road]?, ErrorData?) = DataParser.parseJson(data: data)
+            if let roadData = parsingResult.0 {
+                XCTAssertEqual(roadData[0].displayName, "A3")
+                XCTAssertEqual(roadData[0].statusSeverity, "Good")
+                XCTAssertEqual(roadData[0].statusSeverityDescription, "No Exceptional Delays")
+            }else {
+                XCTFail()
+            }
+        }else {
+            XCTFail()
+        }
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testClient() {
+        let client = Client()
+        client.fetchRemoteData(request: url!, dataHandler: .roadHandler, completion: {data, error in
+            guard error == nil else {
+                XCTFail()
+                return
+            }
+            guard let data = data as? [Road] else {
+                XCTFail()
+                return
+            }
+            
+            let roadData = data[0]
+            XCTAssertEqual(roadData.displayName, "A3")
+            XCTAssertEqual(roadData.statusSeverity, "Good")
+            XCTAssertEqual(roadData.statusSeverityDescription, "No Exceptional Delays")
+        })
     }
     
 }
