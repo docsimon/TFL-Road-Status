b17c341b3956a75ec25f513fcea755073c89e611
doc <doc@docs-MBP.lan>
2018-05-20 15:16:58 +0100

    Created protocols to inject session into the Client

---
+++ TFLRoadStatus/TestModel/URLSessionDataTaskProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  URLSessionDataTaskProtocol.swift
+//  TFLRoadStatus
+//
+//  Created by doc on 20/05/2018.
+//  Copyright © 2018 Simone Barbara. All rights reserved.
+//
+
+import Foundation
+
+protocol URLSessionDataTaskProtocol {
+    func resume()
+}
---
+++ TFLRoadStatus/TestModel/URLSessionProtocol.swift
@@ -0,0 +1,27 @@
+//
+//  URLSessionProtocol.swift
+//  TFLRoadStatus
+//
+//  Created by doc on 20/05/2018.
+//  Copyright © 2018 Simone Barbara. All rights reserved.
+//
+
+import Foundation
+
+protocol URLSessionProtocol {
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol
+}
+
+// MARK: URLSession extension
+// I need to conform URLSession to the URLSessionProtocol in order to use
+// both real session and mock session with the same Client class
+extension URLSession: URLSessionProtocol {
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTaskProtocol{
+        let task = URLSession.shared.dataTask(with: request, completionHandler: completionHandler) as URLSessionDataTask
+        return task as URLSessionDataTaskProtocol
+    }
+}
+
+extension URLSessionDataTask: URLSessionDataTaskProtocol{
+    
+}
